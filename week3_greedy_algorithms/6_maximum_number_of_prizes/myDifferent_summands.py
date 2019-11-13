3850f248e10cbc00c2c5f3756d986df512aad079
gbvsilva <graco.babeuf18@gmail.com>
2019-11-12 22:15:34 -0300

    Update Week 3

--- week3_greedy_algorithms/6_maximum_number_of_prizes/myDifferent_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/myDifferent_summands.py
@@ -1,6 +1,5 @@
 # Uses python3
 import sys
-#import time
 
 def optimal_summands(n):
 	summands = []
@@ -8,23 +7,18 @@ def optimal_summands(n):
 	if n <= 2:
 		return [n]
 	rest = 0
-	for i in range(1, n):
-		if sum(summands)+i > n:
-			rest = n - sum(summands)
-			break
+	i = 1
+	while i*2 < n:
 		summands.append(i)
-
-	summands[-1] += rest
+		n -= i
+		i += 1
+	summands.append(n)
 	return summands
 
 if __name__ == '__main__':
 	input = sys.stdin.read()
 	n = int(input)
-	start = time.time()
-	elapsed = 0
 	summands = optimal_summands(n)
 	print(len(summands))
 	for x in summands:
 		print(x, end=' ')
-	#elapsed = time.time() - start
-	#print("Time -> "+str(elapsed))
